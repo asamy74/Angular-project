64f7306777ccf4328a3bb0baed0a1748d34ffb1e
Ahmed samy <asamy13579@gmail.com>
2026-06-26 17:27:39 +0300

    our work

---
+++ src/app/componants/login.ts
@@ -0,0 +1,80 @@
+import { Component } from '@angular/core';
+import { 
+  FormControl,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule
+} from '@angular/forms';
+
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    ReactiveFormsModule
+  ],
+  templateUrl: './login.html',
+  styleUrls: ['./login.css']
+})
+
+
+export class Login {
+
+
+  loginForm = new FormGroup({
+
+    email: new FormControl('', [
+
+      Validators.required,
+      Validators.email
+
+    ]),
+
+
+    password: new FormControl('', [
+
+      Validators.required,
+      Validators.minLength(6)
+
+    ])
+
+  });
+
+
+
+  get email(){
+
+    return this.loginForm.get('email');
+
+  }
+
+
+
+  get password(){
+
+    return this.loginForm.get('password');
+
+  }
+
+
+
+  submit(){
+
+
+    if(this.loginForm.valid){
+
+      console.log(this.loginForm.value);
+
+    }
+
+    else{
+
+      this.loginForm.markAllAsTouched();
+
+    }
+
+
+  }
+
+
+
+}
